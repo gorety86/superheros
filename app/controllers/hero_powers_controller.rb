50545dfe73a50464f4380eddf46b83260eed87f4
gorety <wagutugorety@gmail.com>
2023-07-02 16:06:34 +0300

    update powers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,7 @@
 class HeroPowersController < ApplicationController
+
+    def create
+        hero_power = HeroPower.create!(hero_power_params)
+        render json: hero_power, status: :created
+    end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,19 +1,38 @@
 class PowersController < ApplicationController
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
     def index
-        powers Power.all
+        powers = Power.all
         render json: powers
     end
 
     def show
-        power = Power.find(params[:id])
+        power = find_power
         render json: power
     end
 
+    def update
+        power = find_power
+        power.update!(power_params)
+        render json: power, status: :ok
+    end
+
     private
+     def find_power
+        Power.find(params[:id])
+    end
+
+    def power_params
+        params.permit(:name, :description)
+    end
+
     def render_not_found_response
         render json: { errors: "Power not found"}, status: :not_found
+    end
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
