45d2f5ae9ba19e43d4ba91e3ffe6a5ceb4a32405
gorety <wagutugorety@gmail.com>
2023-07-02 15:15:07 +0300

    created serializers

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,7 +1,19 @@
 class HerosController < ApplicationController
 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index
         heros = Hero.all
         render json: heros
+    end
+
+    def show
+        hero = Hero.find (params[:id])
+        render json: hero, status: :ok, serializer: ShowHeroPowerSerializer
+    end
+
+
+    def render_not_found_response
+        render json: {errors: "Hero not found"}, status: :not_found
     end
 end
