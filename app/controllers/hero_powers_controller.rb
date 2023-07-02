a2fd9e5ee2b35605f7ee21ddcc583401d2f3c6fb
gorety <wagutugorety@gmail.com>
2023-07-02 16:09:15 +0300

    created superheros

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,7 +1,19 @@
 class HeroPowersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def create
         hero_power = HeroPower.create!(hero_power_params)
         render json: hero_power, status: :created
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { errors: "Hero Power not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
